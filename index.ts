7d2c4e7f63793b180bc2fff358ee1ddfa530c130
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-12-26 04:02:05 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Password manager
+"use strict";
+
+import baseX	from "base-x";
+
+// Init pre-import variables
+const ALPHA79 = "123456789" +
+				"ABCDEFGHJKLMNPQRSTUVWXYZ" +
+				"abcdefghijkmnopqrstuvwxyz" +
+				"!@#$%^&*()_+-=\[]/?<>.";
