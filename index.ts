77ae9333f4e27f6040d57f05b18100a4a5fec002
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-12-26 04:17:45 +0000

    More dummy code

--- index.ts
+++ index.ts
@@ -1,10 +1,28 @@
 // Password manager
 "use strict";
 
-import baseX	from "base-x";
+// Imports
+import baseX		from "base-x";
+import {createHash}	from "crypto";
 
-// Init pre-import variables
 const ALPHA79 = "123456789" +
 				"ABCDEFGHJKLMNPQRSTUVWXYZ" +
 				"abcdefghijkmnopqrstuvwxyz" +
 				"!@#$%^&*()_+-=\[]/?<>.";
+
+const base79 = baseX(ALPHA79);
+
+console.log(makePass("yo", "noob.com"));
+
+function makePass(master: string, loginfor: string): string {
+	return	base79.encode(
+				Buffer.from(
+					hash(master + loginfor)));
+}
+
+// "Borrowed" from... myself?
+// Seriously, this code is from Carbonado.js
+function hash(input: any, format?: string): string {
+	return createHash("sha256")
+		.update(input).digest(<any>(format ? format : "hex"));
+}
